da63d7914d68d6211a04ad288050168b24848722
moteo <moteoo@gmail.com>
2018-02-20 00:44:10 +0300

    100% !!!

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var zarr = [,];
+  preferences = zarr.concat(preferences);
+  var count = 0;
+  for(var i = 1; i <= preferences.length; i++) {
+  var first = preferences[i];//2
+  var second = preferences[first];//3
+  var third = preferences[second];//1
+     if (
+          (third == i) &&
+          (preferences[first] != first &&
+           preferences[second] != second &&
+           preferences[third] != third) )
+     {
+       count++
+       //console.log(first, "first", second, "second", third, "third");
+     }
+  }
+console.log(count, "count", preferences.length);
+return count/3;
 };
